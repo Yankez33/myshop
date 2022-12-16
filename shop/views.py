edb90500dc06317e81cb5ccaf14824d48ec293a2
admin <maycocktim@gmail.com>
2022-12-16 15:49:27 -0800

    Modified models, urls and views

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)  # filtered to retrieve only available products
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)  # slug is option top filter products by category
+        products = products.filter(category=category)
+    return render(request, 'shop/product/list.html',
+                  {'category': category, 'categories': categories, 'products': products})
+
+
+# function to retrieve and display a single product. Product is retrievable thru id as it's unique
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'shop/product/detail.html', {'product': product})
