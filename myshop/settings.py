4761758ddba26605db59b2fe6834ec51983417e0
admin <maycocktim@gmail.com>
2022-12-16 16:51:05 -0800

    Added html files, css files and modified settings & URLs

--- myshop/settings.py
+++ myshop/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'  # deals with media files uploaded by users
+MEDIA_ROOT = BASE_DIR / 'media'  # local path where the media files reside
